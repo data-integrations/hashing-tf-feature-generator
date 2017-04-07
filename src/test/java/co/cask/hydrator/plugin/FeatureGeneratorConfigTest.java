23be689523e124554db32f3ba817ee12de0e4723
Nishith Nand <nishith.nand@gmail.com>
2017-04-06 21:53:12 -0700

    Revert "Un-ignoring the unit test."

--- src/test/java/co/cask/hydrator/plugin/FeatureGeneratorConfigTest.java
+++ src/test/java/co/cask/hydrator/plugin/FeatureGeneratorConfigTest.java
@@ -28,6 +28,7 @@ public class FeatureGeneratorConfigTest {
   private static final Schema INPUT = Schema.recordOf("input", Schema.Field.of("offset", Schema.of(Schema.Type.INT)),
                                                       Schema.Field.of("body", Schema.of(Schema.Type.INT)));
 
+  @Ignore
   @Test
   public void testInvalidInputTypeHashingTFFeatureGenerator() {
     HashingTFFeatureGenerator featureGenerator = new HashingTFFeatureGenerator(new HashingTFFeatureGenerator
@@ -41,6 +42,7 @@ public class FeatureGeneratorConfigTest {
     }
   }
 
+  @Ignore
   @Test
   public void testInvalidInputHashingTFFeatureGenerator() {
     HashingTFFeatureGenerator featureGenerator = new HashingTFFeatureGenerator(new HashingTFFeatureGenerator
@@ -55,6 +57,7 @@ public class FeatureGeneratorConfigTest {
     }
   }
 
+  @Ignore
   @Test
   public void testInvalidInputSkipGramTrainer() {
     HashingTFFeatureGenerator featureGenerator = new HashingTFFeatureGenerator(new HashingTFFeatureGenerator
